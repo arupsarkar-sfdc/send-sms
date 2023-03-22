c5501ca418e010387d1cd1c8e3628d19d51bb7b1
arupsarkar-sfdc <arup.sarkar@salesforce.com>
2023-03-22 12:00:46 -0400

    Added process env port first and then 8082

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 const axios = require('axios')
 const app = express();
 
-const port = 8082 || process.env.port;
+const port = process.env.PORT || 8082;
 
 require("dotenv").config()
 
